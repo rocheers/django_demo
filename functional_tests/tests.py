4ae2121021dd2e5464e5651a65cdd61b60b73713
Yuan Zhang <yuan.vergil@gmail.com>
2016-08-29 12:20:24 -0700

    refactor template to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,8 +7,8 @@ import unittest
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(5)
+        self.browser = webdriver.Chrome('/Users/Vergil/bin/chromedriver')
+        # self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -48,7 +48,7 @@ class NewVisitorTest(LiveServerTestCase):
             '2: Use peacock feathers to make a fly')
 
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome('/Users/Vergil/bin/chromedriver')
 
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
@@ -66,6 +66,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
+                               512,
+                               delta=5
+                               )
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
